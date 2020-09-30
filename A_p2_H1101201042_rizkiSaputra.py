538cc766a9aa75f59c2bb4dc092b2c603f207b50
Kulair <72119138+Kulair@users.noreply.github.com>
2020-09-30 11:26:07 +0700

    Add files via upload

---
+++ A_p2_H1101201042_rizkiSaputra.py
@@ -0,0 +1,319 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "selamat datang\n"
+     ]
+    }
+   ],
+   "source": [
+    "#ini adalah fungsi cetak selamat datang\n",
+    "print(\"selamat datang\")\n",
+    "#print(\"di belajar koding\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "selamat datang\n"
+     ]
+    }
+   ],
+   "source": [
+    "\"\"\"\n",
+    "ini adalah multi line comment\n",
+    "pada kelas python\n",
+    "di belajar koding\n",
+    "\"\"\"\n",
+    "print(\"selamat datang\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "jio\n",
+      "3\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 3\n",
+    "b = \"saya\"\n",
+    "\n",
+    "#case sensitive\n",
+    "nama = \"oji\"\n",
+    "Nama = \"ijo\"\n",
+    "\n",
+    "#huruf a-z\n",
+    "umur = 17\n",
+    "UMUR = 20\n",
+    "\n",
+    "#angka\n",
+    "umur2 = 20\n",
+    "um3ur = 21\n",
+    "\n",
+    "#underscore\n",
+    "_nama = \"jio\"\n",
+    "nama_ = \"dia\"\n",
+    "\n",
+    "print(_nama)\n",
+    "print(a)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (<ipython-input-11-43f928cca030>, line 2)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-11-43f928cca030>\"\u001b[0;36m, line \u001b[0;32m2\u001b[0m\n\u001b[0;31m    2umur = 18\u001b[0m\n\u001b[0m        ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "#angka di depan var\n",
+    "2umur = 18"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "can't assign to operator (<ipython-input-12-47ab76bca040>, line 2)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-12-47ab76bca040>\"\u001b[0;36m, line \u001b[0;32m2\u001b[0m\n\u001b[0;31m    umur-ku = 12\u001b[0m\n\u001b[0m                ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m can't assign to operator\n"
+     ]
+    }
+   ],
+   "source": [
+    "#menggunakan dash\n",
+    "umur-ku = 12"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (<ipython-input-13-b535c1d177b7>, line 2)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-13-b535c1d177b7>\"\u001b[0;36m, line \u001b[0;32m2\u001b[0m\n\u001b[0;31m    umur ku = 18\u001b[0m\n\u001b[0m          ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "#menggunakan spasi\n",
+    "umur ku = 18"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#menggunakan lebih 2 kata\n",
+    "nama_ku = \"ida\" #snake case\n",
+    "namaKu = \"dai\" #camel case"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#bilangan bulat: Integer\n",
+    "tinggi = 4\n",
+    "\n",
+    "#bilangan decimal: Float\n",
+    "luas = 4.5\n",
+    "\n",
+    "#text: String\n",
+    "cetak = \"hello, salam kenal\"\n",
+    "\n",
+    "#logika: Boolean\n",
+    "stop = True\n",
+    "stop2 = False\n",
+    "\n",
+    "stop3 = 1\n",
+    "stop4 = 0"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "========================\n",
+      "\tBIODATA\n",
+      "========================\n",
+      "Nama :  oji\n",
+      "Umur : 17\n",
+      "Tinggi :  171.5  cm\n",
+      "Alamat :  jl. ayani no. 15  Kota  Pontianak\n"
+     ]
+    }
+   ],
+   "source": [
+    "nama = \"oji\"\n",
+    "umur = 17\n",
+    "tinggi = 171.5\n",
+    "alamat = \"jl. ayani no. 15\"\n",
+    "kota = \"Pontianak\"\n",
+    "\n",
+    "print(\"========================\")\n",
+    "print(\"\\tBIODATA\")\n",
+    "print(\"========================\")\n",
+    "print(\"Nama : \", nama)\n",
+    "print(\"Umur :\", umur)\n",
+    "print(\"Tinggi : \", tinggi, \" cm\")\n",
+    "print(\"Alamat : \", alamat, \" Kota \", kota)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
